123ae48dd1a5899f64b44428dc55aa531680ae51
MUHAMMAD UMER <130055381+umer-qayyum@users.noreply.github.com>
2023-05-12 00:06:00 +0500

    Add files via upload

---
+++ NinjaMail/script.js
@@ -0,0 +1,8 @@
+function ab(){
+    document.querySelector(".modal-body2").classList.add("modi");
+    document.querySelector(".modal-body3").classList.add("modi1");
+  }
+  function bc(){
+    document.querySelector(".modal-body2").classList.remove("modi");
+    document.querySelector(".modal-body3").classList.remove("modi1");
+  }
